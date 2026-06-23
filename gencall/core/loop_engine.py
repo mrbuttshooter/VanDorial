0819b5121e5616891e4c35e0899a6e8d3af6c088
Sergio Elias <sergioelias2002@gmail.com>
2026-06-23 11:33:33 +0300

    Revert "feat(shaper): GMT-anchor diurnal curve + auto tz_offset from node drop zone (v2.2.3)"
    This reverts commit f609bc138692a59d590d8a59c4c6c539a71ea8e7.

--- gencall/core/loop_engine.py
+++ gencall/core/loop_engine.py
@@ -641,10 +641,8 @@ class LoopEngine:
                     "ramp_down_end": int(ramp_down_end),
                     "tz_offset": int(tz_offset),
                 }
-                # GMT-anchored: the curve follows UTC and tz_offset rotates it to
-                # the destination market's local daypart (not the box's clock).
                 hour_rate = self._shaper_target_rate(
-                    profile_view, _time.gmtime().tm_hour)
+                    profile_view, _time.localtime().tm_hour)
                 if hour_rate > 0:
                     effective_rate = hour_rate
 
@@ -924,7 +922,7 @@ class LoopEngine:
         import time as _time
         while not self._shaper_stop.is_set():
             try:
-                hour = _time.gmtime().tm_hour  # GMT-anchored curve (tz_offset shifts it)
+                hour = _time.localtime().tm_hour
                 # Snapshot the items so a concurrent start/stop can't mutate the
                 # dict mid-iteration.
                 for cid, campaign in list(self._campaigns.items()):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gencall",
-    version="2.2.3",
+    version="2.2.2",
     description="GenCall - SIP Traffic Generator",
     packages=find_packages(),
     include_package_data=True,
